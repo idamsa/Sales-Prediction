14720938203a9fb17f15feac601ee48cc86cf3cd
Emma Vayrynen <emmavayry@gmail.com>
2019-11-29 13:19:35 +0100

    MultipleRegressionInR.R

--- MultipleRegressionInR.R
+++ MultipleRegressionInR.R
@@ -2,9 +2,20 @@
 
 #Add library and files#
 library(readr)
+library (caret)
 
 existing<-read.csv("existingproductattributes2017.csv")
 
 #Explore data#
 summary(existing)
 str(existing)
+is.na(existing)
+
+
+##Pre-processing data##
+
+#Dummify the data
+existingdummy <-dummyVars ("~.", data = existing)
+readyData <- data.frame(predict(existingdummy, newdata = existing))
+readyData
+str(readyData)
