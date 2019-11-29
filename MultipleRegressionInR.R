407847481d0f94ad141cbb7f11d629343bad7aa5
Damsa Ioana <damsaioana17@gmail.com>
2019-11-29 13:24:59 +0100

    missing values out,corr matrix

--- MultipleRegressionInR.R
+++ MultipleRegressionInR.R
@@ -2,9 +2,29 @@
 
 #Add library and files#
 library(readr)
+library(caret)
+install.packages("corrplot")
+library(corrplot)
 
 existing<-read.csv("existingproductattributes2017.csv")
 
 #Explore data#
 summary(existing)
 str(existing)
+
+
+
+# Removing Missing Values
+is.na(existing)
+existing$BestSellersRank <- NULL
+
+
+# Builind the correlation matrix
+corrData <- cor(readyData)
+
+# Ptints the correlation matrix
+corrData
+
+
+# Heatmap correlation
+corrplot(corrData)
