f73703e9e45a226637da8bf90a3e10e6c808137d
Damsa Ioana <damsaioana17@gmail.com>
2019-11-29 12:06:49 +0100

    regression

--- MultipleRegressionInR.R
+++ MultipleRegressionInR.R
@@ -3,13 +3,10 @@
 
 library(readr)
 
-<<<<<<< HEAD
+
 existing<-read.csv("existingproductattributes2017.csv")
 
 
-=======
-read.csv("existingproductattributes2017.csv")
-
 
 summary(existing)
->>>>>>> aa52da87f9219f15b50673328961183cf15a0476
+
